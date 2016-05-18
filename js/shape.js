26d50c4781ba2d191bb7d7c5130dd57468817f65
Amit Kumar <amit.k03377@gmail.com>
2016-05-19 09:01:07 +1200

    inital commit

---
+++ js/shape.js
@@ -0,0 +1,19 @@
+var shapesCanvas = document.querySelector('#shapes');
+
+var shapesContext = shapesCanvas.getContext('2d');
+
+// Draw a rectangle
+shapesContext.fillStyle = '#00b9ff';
+shapesContext.fillRect(10, 10, 100, 20);
+shapesContext.fillRect(10, 40, 150, 50);
+
+shapesContext.fillStyle = 'rgba(100,0,0,0.5)';
+shapesContext.fillRect(20, 50, 150, 50);
+
+shapesContext.strokeStyle = '#333333';
+shapesContext.lineWidth= 2;
+shapesContext.moveTo(50, 150);
+shapesContext.lineTo(100, 150);
+shapesContext.lineTo(120,170);
+shapesContext.lineTo(140,130);
+shapesContext.stroke();
